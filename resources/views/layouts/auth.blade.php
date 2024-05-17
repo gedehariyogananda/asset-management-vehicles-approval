ebf9c4311c15a87b54e195299a2bfc3a0094c689
Adiracallysta <ariyoga.ga@gmail.com>
2024-05-17 16:40:43 +0700

    final test

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -24,7 +24,7 @@
     <link href="{{ asset('img/favicon.png') }}" rel="icon" type="image/png">
 </head>
 
-<body class="bg-gradient-primary min-vh-100 d-flex justify-content-center align-items-center">
+<body class="bg-gradient-secondary min-vh-100 d-flex justify-content-center align-items-center">
 
     @yield('main-content')
 
